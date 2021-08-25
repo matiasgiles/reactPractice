16239187f4db4d475187fe1ff93574fd7d863b4e
matiasgiles <Matias1giles@gmail.com>
2021-08-24 22:52:13 -0300

    .

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import {cardData} from "../config/data"
 import ContentRowMovies from "./ContentRowMovies"
-
+import GenresInDb from "./GenresInDb"
 import mandalorian from "../assets/images/mandalorian.jpg"
 
 
@@ -59,88 +59,7 @@ function ContentRowTop (){
 						
 
 						
-						<div className="col-lg-6 mb-4">						
-							<div className="card shadow mb-4">
-								<div className="card-header py-3">
-									<h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
-								</div>
-								<div className="card-body">
-									<div className="row">
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Acción
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Animación
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Aventura
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Ciencia Ficción
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Comedia
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Documental
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Drama
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Fantasia
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Infantiles
-												</div>
-											</div>
-										</div>
-										<div className="col-lg-6 mb-4">
-											<div className="card bg-dark text-white shadow">
-												<div className="card-body">
-													Musical
-												</div>
-											</div>
-										</div>
-
-									</div>
-								</div>
-							</div>
-						</div>
+						<GenresInDb />
 					</div>
 				</div>
 				
--- src/components/GenresInDb.js
+++ src/components/GenresInDb.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, {Component} from "react";
+import Genre from "./Genre"
+class GenresInDb extends Component {
+ 
+ 
+    constructor (){
+        super ()
+        this.state ={
+            genresList: []
+        }
+    }
+    componentDidMount(){
 
-export function GeenresInDb (){
+        fetch('/api/genres')
+        .then(res =>{
+        return res.json()
+        })
+        .then(genres =>{
+        console.log(genres)
+        this.setState({genresList: genres.data})
+        })
+        .catch(error => console.log(error))
+        }
+  render (){
+
+      return (
+        <div className="col-lg-6 mb-4">						
+        <div className="card shadow mb-4">
+            <div className="card-header py-3">
+                <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+            </div>
+            <div className="card-body">
+                <div className="row">
+                 
+                {this.state.genresList.map((genre,index)=>{
+                return <Genre {...genre} key={index} />
+                })}
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+      )
     
+  }
+
+
+
 }
+
+export default  GenresInDb;
